b9e28d70877ca33577b537bfb940169e7ee4ef1b
Fulling <2435406817@qq.com>
2022-04-11 20:49:22 +0800

    undefined

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <!-- vue3可以没有根标签 -->
   <h1>APP组件</h1>
-  <h2>name:{{ person.name }}</h2>
+  <h2 v-show="person.name">name:{{ person.name }}</h2>
   <h2>age:{{ person.age }}</h2>
+  <h2 v-show="person.sex">性别:{{ person.sex }}</h2>
   <h3>type:{{ person.job.type }}</h3>
   <h3>type:{{ person.job.salary }}</h3>
   <button @click="changeInfo">修改</button>
+  <button @click="addsex">添加sex</button>
+  <button @click="delname">删除name</button>
 </template>
 
 <script>
@@ -27,9 +30,17 @@ export default {
       person.job.type = "backend";
       person.job.salary = "60K";
     }
+    function addsex() {
+      person.sex = "男";
+    }
+    function delname() {
+      delete person.name;
+    }
     return {
       person,
       changeInfo,
+      addsex,
+      delname,
     };
   },
 };
